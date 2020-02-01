9c360670d9b033857d53cdf35b0f11beae92c20b
Jackson Oberkrich <jacksonpoberkirch@gmail.com>
2020-02-01 05:02:49 +0000

    Added Switchs

---
+++ Assets/Scripts/Interaction/Switch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField]
+    GameObject switchOn;
+
+    [SerializeField]
+    GameObject switchOff;
+
+    public bool isOn = false;
+
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = switchOff.GetComponent<SpriteRenderer>().sprite; 
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = switchOn.GetComponent<SpriteRenderer>().sprite;
+
+        isOn = true;
+    }
+
+}
