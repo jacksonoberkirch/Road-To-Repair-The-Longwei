7c9a5241b2d3d505bfa496a22ef61380a61b4770
Elizabeth Alvarado <danceswithtime@gmail.com>
2020-02-01 06:59:45 +0000

    MAJOR PUSH- fix player, add particles

---
+++ Assets/Scripts/ParticleSortLayerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ParticleSortLayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Change Top to the layer you want it to display on 
+        // could prob. make a public variable for this
+        GetComponent<Renderer>().sortingLayerName = "Top";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
