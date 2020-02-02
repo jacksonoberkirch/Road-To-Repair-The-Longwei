00ef8b2689aed90788c1b56a01c2ca9a423c1519
Vanessa Hoffmann <hoffmann.vanessa13@gmail.com>
2020-02-02 17:36:36 +0000

    added entry animation

---
+++ Assets/Scripts/Interaction/IntroTrans.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroTrans : MonoBehaviour
+{
+    public Image globalGameJam;
+
+    // Update is called once per frame
+    void Start()
+    {
+        globalGameJam.canvasRenderer.SetAlpha(1.0f);
+        fadeOut();
+    }
+   
+     void fadeOut()
+    {
+        globalGameJam.CrossFadeAlpha(0, 5, false);
+    }
+    
+}
