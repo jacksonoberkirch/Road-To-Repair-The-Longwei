d6f9a8b250b93623eac53d49bbc11a98f1c3653c
Jackson Oberkrich <jacksonpoberkirch@gmail.com>
2020-02-02 18:45:34 +0000

    Changed Background music handling

---
+++ Assets/BGM_Handler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_Handler : MonoBehaviour
+{
+    public AudioSource intro;
+
+    public AudioSource loop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intro.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (intro.isPlaying == false && loop.isPlaying == false)
+        {
+            loop.Play();
+        }
+    }
+}
